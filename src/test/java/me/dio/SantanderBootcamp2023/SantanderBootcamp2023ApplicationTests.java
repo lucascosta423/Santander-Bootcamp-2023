cefd931831e7a71b144184e61d294e7664b56e91
Lucas Costa <lukascosta.ls17@gmail.com>
2023-10-22 17:22:38 -0300

    Initial commit

---
+++ src/test/java/me/dio/SantanderBootcamp2023/SantanderBootcamp2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.SantanderBootcamp2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcamp2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
